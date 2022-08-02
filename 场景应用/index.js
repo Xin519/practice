7f54430ea1a63bea91cf1ac3cfa2bc0c264f01c3
BLEACH <3027421343@qq.com>
2022-08-02 14:52:07 +0800

    2022/8/2

--- 场景应用/index.js
+++ 场景应用/index.js
@@ -118,37 +118,176 @@ const childNum = (num, count) => {
 			curIndex = 0
 		};
 	}
-	
-	for(let i in allplayer) {
-		if(allplayer[i] !== 0) return allplayer[i]
+
+	for (let i in allplayer) {
+		if (allplayer[i] !== 0) return allplayer[i]
 	}
 
 }
 
 // Proxy 数据响应式
 let onWatch = (obj, setBind, getLogger) => {
-  let handler = {
-    get(target, property, receiver) {
-      getLogger(target, property)
-      return Reflect.get(target, property, receiver)
-    },
-    set(target, property, value, receiver) {
-      setBind(value, property)
-      return Reflect.set(target, property, value)
-    }
-  }
-  return new Proxy(obj, handler)
+	let handler = {
+		get(target, property, receiver) {
+			getLogger(target, property)
+			return Reflect.get(target, property, receiver)
+		},
+		set(target, property, value, receiver) {
+			setBind(value, property)
+			return Reflect.set(target, property, value)
+		}
+	}
+	return new Proxy(obj, handler)
 }
-let obj = { a: 1 }
+let obj = {
+	a: 1
+}
 let p = onWatch(
-  obj,
-  (v, property) => {
-    console.log(`监听到属性${property}改变为${v}`)
-  },
-  (target, property) => {
-    console.log(`'${property}' = ${target[property]}`)
-  }
+	obj,
+	(v, property) => {
+		console.log(`监听到属性${property}改变为${v}`)
+	},
+	(target, property) => {
+		console.log(`'${property}' = ${target[property]}`)
+	}
 )
 p.a = 2 // 监听到属性a改变
 p.a // 'a' = 2
 
+
+// 双向数据绑定
+let obj = {}
+let input = document.getElementById('input')
+let span = document.getElementById('span')
+// 数据劫持
+Object.defineProperty(obj, 'text', {
+	configurable: true,
+	enumerable: true,
+	get() {
+		console.log('获取数据了')
+	},
+	set(newVal) {
+		console.log('数据更新了')
+		input.value = newVal
+		span.innerHTML = newVal
+	}
+})
+// 输入监听
+input.addEventListener('keyup', function(e) {
+	obj.text = e.target.value
+})
+
+// 字符串出现的不重复最长长度
+// 用一个滑动窗口装没有重复的字符，枚举字符记录最大值即可。用 map 维护字符的索引，遇到相同的字符，把左边界移动过去即可。挪动的过程中记录最大长度：
+
+var lengthOfLongestSubstring = function(s) {
+	let map = new Map();
+	let i = -1
+	let res = 0
+	let n = s.length
+	for (let j = 0; j < n; j++) {
+		if (map.has(s[j])) {
+			i = Math.max(i, map.get(s[j]))
+		}
+		res = Math.max(res, j - i)
+		map.set(s[j], j)
+	}
+	return res
+};
+
+//  setTimeout 实现 setInterval
+function mySetInterval(fn, timeout) {
+	// 控制器，控制定时器是否继续执行
+	var timer = {
+		flag: true
+	};
+	// 设置递归函数，模拟定时器执行。
+	function interval() {
+		if (timer.flag) {
+			fn();
+			setTimeout(interval, timeout);
+		}
+	}
+	// 启动定时器
+	setTimeout(interval, timeout);
+	// 返回控制器
+	return timer;
+}
+
+// 判断对象是否存在循环引用
+const isCycleObject = (obj, parent) => {
+	const parentArr = parent || [obj];
+	for (let i in obj) {
+		if (typeof obj[i] === 'object') {
+			let flag = false;
+			parentArr.forEach((pObj) => {
+				if (pObj === obj[i]) {
+					flag = true;
+				}
+			})
+			if (flag) return true;
+			flag = isCycleObject(obj[i], [...parentArr, obj[i]]);
+			if (flag) return true;
+		}
+	}
+	return false;
+}
+
+
+const a = 1;
+const b = {
+	a
+};
+const c = {
+	b
+};
+const o = {
+	d: {
+		a: 3
+	},
+	c
+}
+o.c.b.aa = a;
+
+console.log(isCycleObject(o))
+
+// 查找有序二维数组的目标值
+var findNumberIn2DArray = function(matrix, target) {
+	if (matrix == null || matrix.length == 0) {
+		return false;
+	}
+	let row = 0;
+	let column = matrix[0].length - 1;
+	while (row < matrix.length && column >= 0) {
+		if (matrix[row][column] == target) {
+			return true;
+		} else if (matrix[row][column] > target) {
+			column--;
+		} else {
+			row++;
+		}
+	}
+	return false;
+};
+
+// 二维数组斜向打印
+function printMatrix(arr) {
+	let m = arr.length,
+		n = arr[0].length
+	let res = []
+
+	// 左上角，从0 到 n - 1 列进行打印
+	for (let k = 0; k < n; k++) {
+		for (let i = 0, j = k; i < m && j >= 0; i++, j--) {
+			res.push(arr[i][j]);
+		}
+	}
+
+	// 右下角，从1 到 n - 1 行进行打印
+	for (let k = 1; k < m; k++) {
+		for (let i = k, j = n - 1; i < m && j >= 0; i++, j--) {
+			res.push(arr[i][j]);
+		}
+	}
+	return res
+}
